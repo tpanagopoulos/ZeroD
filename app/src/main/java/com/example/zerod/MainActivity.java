d563f957cdefb6cb249174e501f1ea688aa43d4a
tpanagopoulos <thodoris.panagopoulos@gmail.com>
2020-04-25 23:06:04 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/zerod/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.zerod;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url = "http://connectu.vodacom.co.za/";
+        Intent browserIntent =
+                new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(browserIntent);
+
+    }
+
+}
